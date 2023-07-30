c82fdddae013804156556277427f073836eeb836
Ian Merritt <iangrin.coding@gmail.com>
2023-07-30 13:04:22 -0600

    adding cart context and shopping icon as well as cart dropdown

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,6 +1,6 @@
 import "./button.styles.scss";
 
-const BUTTON_TYPES_CLASSES = {
+export const BUTTON_TYPE_CLASSES = {
   google: "google-sign-in",
   inverted: "inverted",
 };
@@ -8,7 +8,7 @@ const BUTTON_TYPES_CLASSES = {
 const Button = ({ children, buttonType, ...otherProps }) => {
   return (
     <button
-      className={`button-container ${BUTTON_TYPES_CLASSES[buttonType]}`}
+      className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
       {...otherProps}
     >
       {children}
