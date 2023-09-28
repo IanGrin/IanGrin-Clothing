d6b49db35babdcd3bf1365f6f3a96eb5cb7ac83f
Ian Merritt <iangrin.coding@gmail.com>
2023-09-28 16:02:27 -0600

    fixed media query for individual categorys

--- src/routes/category/category.styles.tsx
+++ src/routes/category/category.styles.tsx
@@ -5,6 +5,12 @@ export const CategoryContainer = styled.div`
   grid-template-columns: repeat(4, 1fr);
   column-gap: 20px;
   row-gap: 50px;
+
+  @media screen and (max-width: 800px) {
+    grid-template-columns: 1fr 1fr;
+    grid-column-gap: 15px;
+    grid-row-gap: 25px;
+  }
 `;
 
 export const Title = styled.h2`
