91560f40c6ba5ea37bf41e55bec4a5bab4d09dbe
Ian Merritt <iangrin.coding@gmail.com>
2023-09-28 15:58:28 -0600

    fixed media query for directory items

--- src/components/directory-item/directory-item.styles.tsx
+++ src/components/directory-item/directory-item.styles.tsx
@@ -36,6 +36,10 @@ export const Body = styled.div`
     font-weight: lighter;
     font-size: 16px;
   }
+
+  @media screen and (max-width: 800px) {
+    width: 145px;
+  }
 `;
 
 export const DirectoryItemContainer = styled.div`
